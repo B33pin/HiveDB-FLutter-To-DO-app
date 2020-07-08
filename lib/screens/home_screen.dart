3b19f6f8e8001156de2705e29988a28bfcd10c0e
B33pin <bipin.bhandari1510@gmail.com>
2020-07-08 14:27:19 +0545

    final release of to-do app with hive db

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hivetodo/models/task_model.dart';
-
 import 'bottom_sheet.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -74,33 +74,50 @@ class HomeScreen extends StatelessWidget {
                       topLeft: Radius.circular(20.0),
                       topRight: Radius.circular(20.0)),
                 ),
-                child:WatchBoxBuilder(box: Hive.box('notes'), builder: (context, contactsBox) {
-                  return ListView.separated(
-                    physics: BouncingScrollPhysics(),
-                    itemBuilder: (BuildContext context, int index) {
-                      final contact = contactsBox.get(index) as Task;
-                      return ListTile(
-                        title: Text(contact.name, style: TextStyle(
-                          decoration: contact.isdone?TextDecoration.lineThrough:null,
-                        ),),
-                        trailing: Checkbox(activeColor: Colors.lightBlueAccent,value:contact.isdone, onChanged: (value){
-                          check = value;
-                          contactsBox.putAt(
-                            index,
-                            Task(name: contact.name, isdone: check),
-                          );
-                        }),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return Divider();
-                    },
-                    itemCount: contactsBox.length,
-                  );
-                })),
+                child:_buildListView()),
           ),
         ],
       ),
     );
   }
 }
+
+Widget _buildListView() {
+  return WatchBoxBuilder(
+    box: Hive.box('notes'),
+    builder: (context, contactsBox) {
+      return ListView.separated(
+        physics: BouncingScrollPhysics(),
+        separatorBuilder: (BuildContext context, int index){
+          return Divider();
+        },
+        itemCount: contactsBox.length,
+        itemBuilder: (BuildContext context, int index) {
+          final contact = contactsBox.getAt(index) as Task;
+
+          return Slidable(
+            actionPane: SlidableDrawerActionPane(),
+            actionExtentRatio: 0.25,
+            child: ListTile(
+              title: Text(contact.name,style: TextStyle(
+                decoration: contact.isdone?TextDecoration.lineThrough:null
+              ),),
+              trailing: Checkbox(activeColor: Colors.lightBlueAccent,value: contact.isdone, onChanged: (tick){
+                contactsBox.putAt(index, Task(name: contact.name, isdone: tick));
+              }),
+            ),
+            secondaryActions: <Widget>[
+              IconSlideAction(
+                caption: 'Delete',
+                color: Colors.red[400],
+                icon: Icons.delete,
+                onTap: (){
+                  contactsBox.deleteAt(index);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );}
